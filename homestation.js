b660568078e9f39caded97c7d371ef5188cf1d87
terror <terror85@gmail.com>
2020-04-25 22:46:47 +0300

    so

--- homestation.js
+++ homestation.js
@@ -3,9 +3,9 @@ Home Station javascript
 */
 $( document ).ready(function() {
 	console.log( "document loaded" );
-	/*var socket=new WebSocket("/homestationws");
+	var socket=new WebSocket("/homestationws");
 	
-	socket.onmessage = function(event) {
+	/*socket.onmessage = function(event) {
 	alert("Получены данные " + event.data);
 	var myObj = JSON.parse(event.data);
 	if(myObj.response=='updateLayout')
