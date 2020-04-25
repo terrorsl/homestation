8058e8a9db2eb6005744c6da867357bce596920d
terror <terror85@gmail.com>
2020-04-25 22:50:21 +0300

    d

--- homestation.js
+++ homestation.js
@@ -3,7 +3,7 @@ Home Station javascript
 */
 $( document ).ready(function() {
 	console.log( "document loaded" );
-	var socket=new WebSocket("/homestationws");
+	let socket=new WebSocket("/homestationws");
 	
 	/*socket.onmessage = function(event) {
 	alert("Получены данные " + event.data);
