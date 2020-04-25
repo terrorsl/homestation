56a4dc0b76a51536f54bc1a845d26c2de450e2b8
terror <terror85@gmail.com>
2020-04-25 20:10:57 +0300

    test

--- homestation.js
+++ homestation.js
@@ -1,8 +1,20 @@
 /*!
 Home Station javascript
 */
+var socket;
+$( document ).ready(function() {
+	console.log( "document loaded" );
+	socket=new WebSocket("/homestationws");
+	
+	socket.onmessage = function(event) {
+	alert("Получены данные " + event.data);
+	var myObj = JSON.parse(event.data);
+	if(myObj.response=='updateLayout')
+	{AppendCard();}
+	socket.send("command:getLayout");
+	};
+});
 function Test()
 {
   alert('test');
 }
-Test();
